2c9e2ed50158eb2cbe87e44c90358aabc035d7cc
kccnma <gargiulo@hawaii.edu>
2025-01-27 10:09:46 -1000

    init

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# sitebase2
-Source/dev repo for Sitebase2, another minimal HTML + CSS + JS base front-end starter/boilerplate for simple static websites.
+# [SiteBase2](https://kccnma.github.io/sitebase2/ "SiteBase2 Demo and Docs")
+
+Latest development version of Sitebase2, an ongoing, always-a-work-in-progress minimal HTML + CSS + JS base front-end framework for simple web site projects. This is the full dev version that contains the uncompiled source files, documentation, lessons, and post-build pre-compiled static-rendered files in multiple variations for different users and development environments.
+
+## Download
+- [Download SiteBase2 .Zip](https://kccnma.github.io/sitebase2/variations/sitebase2.static "Download SiteBase2 .zip file")
+
+## Lessons
+- [SiteBase2 Step-by-Step Lessons](https://kccnma.github.io/sitebase2/lessons.html "SiteBase Lessons (incomplete)")
+
+## Documentation
+- [SiteBase2 Docs](https://kccnma.github.io/sitebase2/documentation.html "SiteBase Docs (incomplete)")
+
+## Demos
+- [SiteBase Website](https://kccnma.github.io/sitebase2/variations/sitebase2-static/ "SiteBase Website")
+
+## Looking for the original SiteBase (version #1)?
+- [SiteBase1](https://kccnma.github.io/sitebase1/ "SiteBase1 Demo and Docs")
+
+## To Run the full dev version locally
+- WARNING: please note that this source develpment repository is currently under construction and may not install without manually updating multiple dependencies. This is a known issue and on the list of future project plans/goals.
+- this source development version requires [node](https://nodejs.org/en/) and [gulp](https://gulpjs.com/) 
+```
+$ npm install
+$ npm run dev
+```
+
+## Updates
+- 01-24-25
+  - initital migration of source code from SiteBase1
---
+++ gulpfile.js
@@ -0,0 +1,338 @@
+const gulp = require("gulp");
+const { parallel, series } = require("gulp");
+
+const htmlmin = require("gulp-htmlmin");
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
+const concat = require("gulp-concat");
+const browserSync = require("browser-sync").create(); //https://browsersync.io/docs/gulp
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const rename = require("gulp-rename");
+const zip = require('gulp-zip');
+
+// /*
+// TOP LEVEL FUNCTIONS
+//     gulp.task = Define tasks
+//     gulp.src = Point to files to use
+//     gulp.dest = Points to the folder to output
+//     gulp.watch = Watch files and folders for changes
+// */
+
+
+// HTML
+function html(cb) {
+  gulp.src("src/**/*.html")
+    .pipe(gulp.dest("dist"))
+    .pipe(
+      htmlmin({
+        collapseWhitespace: false
+      })
+    )
+    .pipe(gulp.dest("dist"));
+  cb();
+}
+
+// SCSS
+function css(cb) {
+  gulp.src("src/scss/**/*.scss")
+    .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
+    .pipe(autoprefixer({
+      browserlist: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest("dist/css"))
+    // Stream changes to all browsers
+    .pipe(browserSync.stream());
+  cb();
+}
+
+// JS
+function js(cb) {
+  gulp.src("src/js/**/*js")
+    .pipe(babel({
+      presets: ['@babel/preset-env']
+    }))
+    .pipe(concat("script.js"))
+    // .pipe(uglify())  // TO MINIMIZE
+    .pipe(gulp.dest("dist/js"));
+  cb();
+}
+
+// IMAGES
+function imageMin(cb) {
+  gulp.src("src/img/**/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/img"));
+  cb();
+}
+
+// WATCH
+function watch_files() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch("./index.html", html).on("change", browserSync.reload);
+  gulp.watch("src/*.html", html).on("change", browserSync.reload);
+  gulp.watch("src/scss/**/*.scss", css);
+  gulp.watch("src/js/*.js", js).on("change", browserSync.reload);
+  gulp.watch("src/**/*.+(png|jpg|jpeg|gif|svg)", imageMin).on("change", browserSync.reload);
+}
+
+// Default 'gulp' command with start local server and watch files for changes.
+exports.default = series(html, css, js, imageMin, watch_files);
+
+// 'gulp build' will build all assets but not run on a local server.
+exports.build = parallel(html, css, js, imageMin);
+
+//
+//
+// ******* END STANDARD SiteBase GULP CONFIG *******
+//
+//
+
+
+
+
+
+// *****************************************
+// *****************************************
+//
+// ********** BEGIN BUILD DOCS *************
+//
+// *****************************************
+// *****************************************
+
+
+// COPY HTML FROM ROOT SRC
+function copyhtmlroot(cb) {
+  gulp.src("src/**/*.html")
+    .pipe(gulp.dest("docs/variations/sitebase2-static/"))
+    .pipe(gulp.dest("docs/variations/sitebase2-scss/"));
+  cb();
+}
+
+// COPY SASS FROM ROOT SRC
+function copysassroot(cb) {
+  gulp.src("src/**/*.scss")
+    .pipe(gulp.dest("docs/"))
+    .pipe(gulp.dest("docs/variations/sitebase2-scss/"));
+  cb();
+}
+
+// COPY JS FROM ROOT SRC
+function copyjsroot(cb) {
+  gulp.src("src/**/*.js")
+    .pipe(gulp.dest("docs/"))
+    .pipe(gulp.dest("docs/variations/sitebase2-static/"))
+    .pipe(gulp.dest("docs/variations/sitebase2-scss/"))
+    .pipe(gulp.dest("docs/examples/productsite/"))
+    // .pipe(gulp.dest("docs/examples/sitename-singlepagesite/"))
+    .pipe(gulp.dest("docs/examples/singlepageportfolio/"));
+  cb();
+}
+
+// COPY IMAGES FROM ROOT SRC
+function copyimagesroot(cb) {
+  gulp.src("src/**/*.+(png|jpg|jpeg|gif|svg)")
+    .pipe(gulp.dest("docs/"))
+    .pipe(gulp.dest("docs/variations/sitebase2-static/"))
+    .pipe(gulp.dest("docs/variations/sitebase2-scss/"))
+    .pipe(gulp.dest("docs/examples/productsite/"))
+    .pipe(gulp.dest("docs/examples/sitename-singlepagesite/"))
+    .pipe(gulp.dest("docs/examples/singlepageportfolio/"));
+  cb();
+}
+
+// SASS ROOT
+const sassOptions = {
+  outputStyle: "expanded"
+};
+function sassroot(cb) {
+  gulp.src("src/scss/**/*.scss")
+    .pipe(sass(sassOptions))
+    .pipe(gulp.dest("docs/css/"))
+    .pipe(gulp.dest("docs/variations/sitebase2-static/css/"))
+    .pipe(gulp.dest("docs/variations/sitebase2-scss/css/"))
+    .pipe(gulp.dest("docs/examples/productsite/css/"))
+    .pipe(gulp.dest("docs/examples/sitename-singlepagesite/css/"))
+    .pipe(gulp.dest("docs/examples/singlepageportfolio/css/"));
+  cb();
+}
+// SASS LESSON1
+// function loremipsum(cb) {
+//   gulp.src("src/lorem/**/*.scss")
+//     .pipe(gulp.dest("docs/"))
+//     .pipe(gulp.dest("docs/"));
+//   cb();
+// }
+
+// SASS LESSON1
+function sasslesson1(cb) {
+  gulp.src("docs/lessons/base-boilerplate.scss")
+    .pipe(sass(sassOptions))
+    .pipe(rename("css/style.css"))
+    .pipe(gulp.dest("docs/lessons/base-boilerplate/"));
+  cb();
+}
+
+// SASS LESSON2
+function sasslesson2(cb) {
+  gulp.src("docs/lessons/base-content.scss")
+    .pipe(sass(sassOptions))
+    .pipe(rename("css/style.css"))
+    .pipe(gulp.dest("docs/lessons/base-content/"));
+  cb();
+}
+
+// SASS LESSON3
+function sasslesson3(cb) {
+  gulp.src("docs/lessons/base-layout.scss")
+    .pipe(sass(sassOptions))
+    .pipe(rename("css/style.css"))
+    .pipe(gulp.dest("docs/lessons/base-layout/"));
+  cb();
+}
+
+// SASS LESSON4
+function sasslesson4(cb) {
+  gulp.src("docs/lessons/base-site.scss")
+    .pipe(sass(sassOptions))
+    .pipe(rename("css/style.css"))
+    .pipe(gulp.dest("docs/lessons/base-site/"));
+  cb();
+}
+
+// SASS LESSON5
+function sasslesson5(cb) {
+  gulp.src("docs/lessons/base-site-togglenav.scss")
+    .pipe(sass(sassOptions))
+    .pipe(rename("css/style.css"))
+    .pipe(gulp.dest("docs/lessons/base-site-togglenav/"));
+  cb();
+}
+
+// SASS LESSON6
+function sasslesson6(cb) {
+  gulp.src("docs/lessons/base-site-subpage.scss")
+    .pipe(sass(sassOptions))
+    .pipe(rename("css/style.css"))
+    .pipe(gulp.dest("docs/lessons/base-site-subpage/"));
+  cb();
+}
+
+
+// ZIP LESSON1
+function ziplesson1(cb) {
+  gulp.src("docs/lessons/base-boilerplate/**/*")
+    .pipe(zip("base-boilerplate.zip"))
+    .pipe(gulp.dest("docs/lessons/"));
+  cb();
+}
+
+// ZIP LESSON2
+function ziplesson2(cb) {
+  gulp.src("docs/lessons/base-content/**/*")
+    .pipe(zip("base-content.zip"))
+    .pipe(gulp.dest("docs/lessons/"));
+  cb();
+}
+
+// ZIP LESSON3
+function ziplesson3(cb) {
+  gulp.src("docs/lessons/base-layout/**/*")
+    .pipe(zip("base-layout.zip"))
+    .pipe(gulp.dest("docs/lessons/"));
+  cb();
+}
+
+// ZIP LESSON4
+function ziplesson4(cb) {
+  gulp.src("docs/lessons/base-site/**/*")
+    .pipe(zip("base-site.zip"))
+    .pipe(gulp.dest("docs/lessons/"));
+  cb();
+}
+
+// ZIP LESSON5
+function ziplesson5(cb) {
+  gulp.src("docs/lessons/base-site-togglenav/**/*")
+    .pipe(zip("base-site-togglenav.zip"))
+    .pipe(gulp.dest("docs/lessons/"));
+  cb();
+}
+
+// ZIP LESSON6
+function ziplesson6(cb) {
+  gulp.src("docs/lessons/base-site-subpage/**/*")
+    .pipe(zip("base-site-subpage.zip"))
+    .pipe(gulp.dest("docs/lessons/"));
+  cb();
+}
+
+// ZIP VERSION1
+function zipvariation1(cb) {
+  gulp.src("docs/variations/sitebase2-static/**/*")
+    .pipe(zip("sitebase2-static.zip"))
+    .pipe(gulp.dest("docs/variations/"));
+  cb();
+}
+
+// ZIP VARIATION2
+function zipvariation2(cb) {
+  gulp.src("docs/variations/sitebase2-scss/**/*")
+    .pipe(zip("sitebase2-scss.zip"))
+    .pipe(gulp.dest("docs/variations/"));
+  cb();
+}
+
+// ZIP EXAMPLE1
+function zipexample1(cb) {
+  gulp.src("docs/examples/productsite/**/*")
+    .pipe(zip("productsite.zip"))
+    .pipe(gulp.dest("docs/examples/"));
+  cb();
+}
+
+// ZIP EXAMPLE2
+function zipexample2(cb) {
+  gulp.src("docs/examples/singlepageportfolio/**/*")
+    .pipe(zip("singlepageportfolio.zip"))
+    .pipe(gulp.dest("docs/examples/"));
+  cb();
+}
+
+// ZIP EXAMPLE3
+function zipexample3(cb) {
+  gulp.src("docs/examples/sitename-singlepagesite/**/*")
+    .pipe(zip("sitename-singlepagesite.zip"))
+    .pipe(gulp.dest("docs/examples/"));
+  cb();
+}
+
+// WATCH DOCS
+const doc_series = [html, css, js, imageMin, copyhtmlroot, copysassroot, copyjsroot, copyimagesroot, sassroot, sasslesson1, sasslesson2, sasslesson3, sasslesson4, sasslesson5, sasslesson6, ziplesson1, ziplesson2, ziplesson3, ziplesson4, ziplesson5, ziplesson6, zipvariation1, zipvariation2, zipexample1, zipexample2, zipexample3];
+
+function watch_docs() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch("src/*.html", series(doc_series)).on("change", browserSync.reload);
+  gulp.watch("src/scss/**/*.scss", series(doc_series));
+  gulp.watch("src/js/*.js", series(doc_series)).on("change", browserSync.reload);
+  gulp.watch("src/**/*.+(png|jpg|jpeg|gif|svg)", series(doc_series)).on("change", browserSync.reload);
+  gulp.watch("docs/*.html", series(doc_series)).on("change", browserSync.reload);
+}
+
+
+exports.docs = series(doc_series, watch_docs);
+
+//
+// END BUILD DOCS
+//
+
